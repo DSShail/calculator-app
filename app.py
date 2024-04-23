b87450d78e2a8ca7481df63d06b1d19388172502
shailesh <shaileshvbs0@gmail.com>
2024-04-23 17:31:52 +0530

    added rectangle area calculation

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from calc_func import do_addition,do_substraction
+from multiply import do_multiplication
 
 def main():
     print("Welcome to the calculator app")
     print("""\n select a function from the given choices
           1. Add
           2. Substract
+          3. Multiplication
             """)
     user_input=int(input('Select a function'))
     a=int(input('Enter the value of A '))
@@ -16,6 +18,11 @@ def main():
     elif user_input==2:
         result=do_substraction(a,b)
         print("The substraction is {}".format(result))
+
+    elif user_input==3:
+        result=do_multiplication(a,b)
+        print("The multiplcication is {}".format(result))
+        
 
 
 if __name__=="__main__":
---
+++ multiply.py
@@ -0,0 +1,4 @@
+
+
+def do_multiplication(a:int,b:int):
+    return a*b
